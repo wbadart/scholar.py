cfad15a60fda37bdfd247250bdca1e792f376b98
Will Badart <wbadart@live.com>
2018-02-23 13:11:03 -0500

    Declared scholar as package in setup

--- setup.py
+++ setup.py
@@ -10,13 +10,14 @@ from setuptools import find_packages, setup
 
 setup(name='scholar',
       version='2.11.0',
+      packages=['scholar'],
       py_modules=['scholar'],
       install_requires=[
           'beautifulsoup4',
       ],
       entry_points={
           'console_scripts': [
-              'scholar = scholar:main'
+              'gscholar = scholar:main'
           ],
       },
       zip_safe=False)
